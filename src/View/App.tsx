97843a8e7e8c831c9be51634cad898315a43b1a2
gongping1990 <wx_2d4ee0328b904aa789f1889f4721f6f3@git.code.tencent.com>
2019-09-27 15:45:21 +0800

    处理接口数据

--- src/View/App.tsx
+++ src/View/App.tsx
@@ -51,7 +51,7 @@ class App extends React.Component<{}, State> {
     } else if (name) {
       await api.user
         .loginWithPhone({
-          phone: '13699011543',
+          phone: '15884594704',
           code: '123654'
         })
         .then(({ data }) => {
